f4070a35913db705953e8c4afd8b7dc70ccf45fd
subract <self@subract.dev>
2022-09-18 07:09:02 -0500

    Split i3 config, fix polybar multimonitor
    i3 config is now split between two files - one with most of the custom config,
    one with the default stuff. Polybar launching has been reworked to get the tray
    to consistently start on the appropriate screen, although I'm sure I'll have to
    rework the script to work with the laptop.
    
    
    Former-commit-id: e3022974296ceb3fd732b1249f53efb5b021e01a

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-# Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+# https://github.com/polybar/polybar/issues/763#issuecomment-450940924
+# Launches with tray on DP-4 if connected, DP-2 otherwise
+(
+  flock 200
 
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-# polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+  killall -q polybar
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload mybar &
-done
+  while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
 
-echo "Polybar launched..."
+  outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  tray_output=DP-2
+
+  for m in $outputs; do
+    if [[ $m == "DP-4" ]]; then
+        tray_output=$m
+    fi
+  done
+
+  for m in $outputs; do
+    export MONITOR=$m
+    export TRAY_POSITION=none
+    if [[ $m == $tray_output ]]; then
+      TRAY_POSITION=right
+    fi
+
+    polybar --reload mybar </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    disown
+  done
+) 200>/var/tmp/polybar-launch.lock
